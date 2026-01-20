ea072fc2252f91c747f7beaf236f3204fb231bee
Oscar Torres <oscardtorres@hotmail.com>
2026-01-20 00:25:23 -0500

    chore: update stack name to ClarityAppStack in app.ts

--- infra/cdk/bin/app.ts
+++ infra/cdk/bin/app.ts
@@ -3,4 +3,4 @@ import { App } from "aws-cdk-lib";
 import { CoreStack } from "../lib/stack";
 
 const app = new App();
-new CoreStack(app, "CoupleExpensesStack");
+new CoreStack(app, "ClarityAppStack");
